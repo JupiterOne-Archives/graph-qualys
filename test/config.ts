12fa22696fe31900055dfe5b3ce5c1c474f8c3c2
Adam Williams <adam.williams@jupiterone.io>
2020-10-08 17:40:23 -0400

    Default config for CI/CD

--- test/config.ts
+++ test/config.ts
@@ -2,7 +2,7 @@ import { QualysIntegrationConfig } from '../src/types';
 
 export const config: QualysIntegrationConfig = {
   qualysApiUrl:
-    process.env.QUALYS_API_URL || 'https://BLAH.qg3.apps.qualys.com',
+    process.env.QUALYS_API_URL || 'https://qualysapi.qg3.apps.qualys.com',
   qualysPassword: process.env.QUALYS_PASSWORD || 'password',
-  qualysUsername: process.env.QUALYS_USERNAME || 'username',
+  qualysUsername: process.env.QUALYS_USERNAME || 'upter3aw',
 };
