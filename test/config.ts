9060f51df4fceaee8cf5ef83e8cf168ce371320e
Chris Elias <chris.elias5@gmail.com>
2022-03-09 16:20:22 -0500

    Updating the recordings

--- test/config.ts
+++ test/config.ts
@@ -3,13 +3,21 @@ import {
   DEFAULT_SCANNED_SINCE_DAYS,
   DEFAULT_VMDR_FINDING_RESULT_QIDS,
 } from '../src/constants';
+import * as dotenv from 'dotenv';
+import * as path from 'path';
 import { QualysIntegrationConfig } from '../src/types';
+
+if (process.env.LOAD_ENV) {
+  dotenv.config({
+    path: path.join(__dirname, '../.env'),
+  });
+}
 
 export const config = {
   qualysApiUrl:
     process.env.QUALYS_API_URL || 'https://qualysapi.qg3.apps.qualys.com',
   qualysPassword: process.env.QUALYS_PASSWORD || 'password',
-  qualysUsername: process.env.QUALYS_USERNAME || 'upter3aw',
+  qualysUsername: process.env.QUALYS_USERNAME || 'upter3sa',
   minScannedSinceDays: DEFAULT_SCANNED_SINCE_DAYS,
   minFindingsSinceDays: DEFAULT_FINDINGS_SINCE_DAYS,
   vmdrFindingResultQidNumbers: DEFAULT_VMDR_FINDING_RESULT_QIDS,
